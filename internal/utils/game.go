5bf29d07316ff1c3767f6ed050f60d5e1759e50e
CheckM4te <chkmtd@gmail.com>
2023-11-01 13:51:59 +0100

    Add neighborCount and exists functions

--- internal/utils/game.go
+++ internal/utils/game.go
@@ -7,20 +7,46 @@ type Game struct {
 	Grid     [][]*Cell
 }
 
-func (g *Game) GetNeighbor(c Cell, x int, y int) *Cell {
+func (g *Game) GetNeighbor(c *Cell, x int, y int) *Cell {
 	cX := c.Position.X
 	cY := c.Position.Y
 
 	if cX+x < 0 || cX+x >= g.Width {
-		return g.Grid[0][0]
+		return c
 	}
 	if cY+y < 0 || cY+y >= g.Height {
-		return g.Grid[0][0]
+		return c
 	}
 
 	return g.Grid[cX+x][cY+y]
 }
 
+func (g *Game) Exists(x, y int) bool {
+	return x >= 0 && x < len(g.Grid) && y >= 0 && y < len(g.Grid[x])
+}
+
 func (g *Game) GetCell(x, y int) *Cell {
-	return g.Grid[x][y]
+	if g.Exists(x, y) {
+		return g.Grid[x][y]
+	}
+	return g.GetCell(0, 0)
+}
+
+func (g *Game) GetNumberAliveNeighbors(c *Cell) int {
+	var neighbors int
+
+	y := c.Position.Y
+	x := c.Position.X
+
+	for i := y - 1; i <= y+1; i++ {
+		for j := x - 1; j <= x+1; j++ {
+			if i == y && j == x {
+				continue
+			}
+			if g.GetCell(j, i).Alive {
+				neighbors++
+			}
+		}
+	}
+	return neighbors
 }
--- internal/utils/update.go
+++ internal/utils/update.go
@@ -4,7 +4,7 @@ func (g *Game) Update() {
 	for i := range g.Grid {
 		for j := range g.Grid[i] {
 			c := g.Grid[i][j]
-			if g.GetNeighbor(*c, 1, -1).Alive {
+			if g.GetNumberAliveNeighbors(c) <= 3 {
 				c.Alive = !c.Alive
 			}
 		}

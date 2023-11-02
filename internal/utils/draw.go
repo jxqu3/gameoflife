9e8ba661c4a3095b86c6ecf33c42f32722b88e2f
CheckM4te <chkmtd@gmail.com>
2023-11-02 07:16:15 +0100

    grid outline + seemingy fixed multi selection bug

--- internal/utils/draw.go
+++ internal/utils/draw.go
@@ -9,7 +9,7 @@ import (
 func (g *Game) Draw() {
 	cs := g.CellSize
 
-	rl.ClearBackground(color.RGBA{0, 0, 0, 255})
+	rl.ClearBackground(color.RGBA{20, 20, 20, 255})
 
 	for x := range g.Grid.Cells {
 		for y := range g.Grid.Cells[x] {
@@ -19,7 +19,7 @@ func (g *Game) Draw() {
 			if c.Alive {
 				c.Color = color.RGBA{uint8(float64(g.GetNumberAliveNeighbors(c)) / 4 * 255), 127, 0, 255}
 			}
-			if rl.GetMouseX() >= int32(x*cs) && rl.GetMouseX() <= int32(x*cs+cs) && rl.GetMouseY() >= int32(y*cs) && rl.GetMouseY() <= int32(y*cs+cs) {
+			if rl.GetMouseX() >= int32(x*cs) && rl.GetMouseX() <= int32(x*cs+cs-1) && rl.GetMouseY() >= int32(y*cs) && rl.GetMouseY() <= int32(y*cs+cs-1) {
 				if rl.IsMouseButtonDown(rl.MouseLeftButton) {
 					if !c.JustChanged {
 						c.Alive = !g.GetCell(x, y).Alive
@@ -34,7 +34,7 @@ func (g *Game) Draw() {
 				}
 				c.Color = color.RGBA{255, 255, 255, 255}
 			}
-			rl.DrawRectangle(int32(x*cs), int32(y*cs), int32(cs), int32(cs), c.Color)
+			rl.DrawRectangle(int32(x*cs+1), int32(y*cs+1), int32(cs-2), int32(cs-2), c.Color)
 
 		}
 	}
